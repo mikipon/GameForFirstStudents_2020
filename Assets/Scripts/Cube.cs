5df6f1670fcb444699ae6697a5034476e6fb8728
okajimaKota <kouta0325byd@icloud.com>
2021-01-11 20:36:41 +0900

    メッセージ出力変更

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,7 +7,7 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello!");
+        Debug.Log("World!");
     }
 
     // Update is called once per frame
