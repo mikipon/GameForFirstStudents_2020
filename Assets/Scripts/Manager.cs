41e6b79e5ae4c580e906512c1c099269e36cc5d5
okajimaKota <kouta0325byd@icloud.com>
2021-03-12 01:38:20 +0900

    EnemyTimerクラス、Playerクラス、Managerクラス追加

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static int fase;//ゲーム進行度
+    public List<GameObject> enemy = new List<GameObject>();//現ステージ残存エネミー
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
