7a73d1f8633df931fb31366d779984464ad436fe
okajimaKota <kouta0325byd@icloud.com>
2021-04-06 03:49:54 +0900

    タイトルのステッカーの当たり判定調節、ステッカーの一部画像改良

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -14,7 +14,7 @@ public class TitleManager : MonoBehaviour
     public bool bol        = true;
 
     //ロード時間
-    public int loadingTime = 5;
+    public int loadingTime;
 
     //クリック回数用
     int click = 0;
@@ -35,7 +35,7 @@ public class TitleManager : MonoBehaviour
     {
 
         Debug.Log("Start");
-        int cleateTotal = 100;
+        int cleateTotal = 125;
 
         //ステッカーの生成するメソッド
         for (int i=0; i< cleateTotal; i++)
@@ -43,8 +43,27 @@ public class TitleManager : MonoBehaviour
             int xRam   = Random.Range(-9, 10);//生成位置Xをランダム
             int stiRam = Random.Range(0, stickers.Length);//生成するステッカーをランダム
 
-            Instantiate(obj, new Vector2(xRam, 8), Quaternion.identity)
-                .GetComponent<SpriteRenderer>().sprite = stickers[stiRam];
+            GameObject temp = Instantiate(obj, new Vector2(xRam, 8), Quaternion.identity);
+            temp.GetComponent<SpriteRenderer>().sprite = stickers[stiRam];
+            temp.GetComponent<Rigidbody2D>().gravityScale = 2;
+
+            //ステッカーによって大きさ変更、デフォルト半径3
+            switch (stiRam)
+            {
+                case 11:
+                case 23:
+                case 24:
+                case 27:
+                    temp.GetComponent<CircleCollider2D>().radius = 2;
+                    break;
+                case 12:
+                case 13:
+                case 14:
+                case 21:
+                case 26:
+                    temp.GetComponent<CircleCollider2D>().radius = 1;
+                    break;
+            }
 
             yield return new WaitForSeconds((float)loadingTime / cleateTotal);
             
